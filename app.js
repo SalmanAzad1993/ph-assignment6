5406893b4582ea012965737a807f42663a67e655
unknown <salmanazad1993@gmail.com>
2021-02-14 19:28:36 +0600

    forth one is complete

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+const searchInput = document.getElementById("search");
 // selected image 
 let sliders = [];
 
@@ -12,6 +13,13 @@ let sliders = [];
 // Find the name in the url and go to their website
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+
+// search using enter key
+searchInput.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter'){
+      searchBtn.click();
+    }
+});
 
 // show images 
 const showImages = (images) => {
